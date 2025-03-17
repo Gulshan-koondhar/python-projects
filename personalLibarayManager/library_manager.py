dd30369c74543e4f40bb19a06c1fd8af511f09ef
Gulshan-koondhar <153608541+Gulshan-koondhar@users.noreply.github.com>
2025-03-17 12:16:25 +0500

    Personal Libraray Manager

--- personalLibarayManager/library_manager.py
+++ personalLibarayManager/library_manager.py
@@ -1,22 +1,17 @@
-
 import json
-
 book_file = "books.txt"
-
 def load_books():
     try:
         with open(book_file, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
-
 def save_books(books):
     try:
         with open(book_file, "w") as file:
             json.dump(books, file)
     except Exception as e:
         print(f"Error saving books: {e}")
-
 def add_book():
     title = input("\nEnter the title of the book: ")
     author = input("Enter the author of the book: ")
@@ -38,7 +33,6 @@ def add_book():
     books.append(book)
     save_books(books)
     print(f"Book added: {title} by {author} successfully!")
-
 def remove_book():
     title = input("\nEnter the title of the book: ")
     books = load_books()
@@ -49,9 +43,7 @@ def remove_book():
             print(f"Book '{title}' removed successfully: ")
             break
     else:
-        print(f"Book not found: {title}")
-   
-    
+        print(f"Book not found: {title}")    
 def search_book():
     search_type = input("\nEnter 1 to search by title, 2 to search by author: ")
     if search_type == "1":
@@ -63,7 +55,6 @@ def search_book():
                 break
         else:
             print(f"Book not found: {title}")
-        
     elif search_type == "2":
         author = input("Enter the author of the book: ")
         books = load_books()
@@ -75,7 +66,6 @@ def search_book():
             print(f"Book written by {author} not found!")
     else:
         print("Invalid search type. Please enter 1 or 2.")
-
 def view_books():
     books = load_books()
     if not books:
@@ -86,7 +76,6 @@ def view_books():
     for book in books:
         print(f"{i+1}. {book['title']} by {book['author']} ({book['year']}) - {book['genre']} - {book['status']}")
         i += 1
-
 def display_statistics():
     books = load_books()
     if not books:
@@ -99,7 +88,6 @@ def display_statistics():
     percentage_read = read / total_books * 100
     print(f"\nTotal books: {total_books}")
     print(f"Percentage of books read: {percentage_read:.2f}%")
-
 def main():
     while True:
         print("\n*** Welcome to your Personal Library Manager! ***")
@@ -123,7 +111,8 @@ def main():
         elif choice == "6":
             print("\n==== Library saved to file. Goodbye!+ ====")
             break
-
+        else:
+            print("Invalid choice. Please enter a number between 1 and 6.")
 if __name__ == "__main__":
     main()
 
